9dd90b2c8602cc906e2967825f35f872b98bd4fb
GustavoEllwanger <gustavobellwanger@gmail.com>
2026-03-13 12:19:57 -0300

    Update ex005.py

--- Semana1/ex005.py
+++ Semana1/ex005.py
@@ -5,18 +5,16 @@ class Funcionario:
         self.cargo = cargo
     
     def calcular_bonus(self):
-        if self.cargo == "Gerente".upper():
-            self.salario *= 1.10
-            print("Novo salário:", self.salario)
+        if self.cargo == "Gerente":
+            return self.salario * 1.10
         else:
-            self.salario *= 1.05
-            print("Novo salário: ", self.salario)
+            return self.salario * 1.05
 
 
-Gerente = Funcionario("Claudio", 5000, "Gerente".upper())
+Gerente = Funcionario("Claudio", 5000, "Gerente")
 Assistente = Funcionario("Ricardo", 1394, "Assistente")
 Estagiario = Funcionario("Gustavo", 800, "Estagiário")
 
-Gerente.calcular_bonus()
-Assistente.calcular_bonus()
-Estagiario.calcular_bonus()
+print(Gerente.nome, "Salário com bônus: ", Gerente.calcular_bonus())
+print(Assistente.nome, "Salário com bônus: ", Assistente.calcular_bonus())
+print(Estagiario.nome, "Salário com bônus: ", Estagiario.calcular_bonus())
